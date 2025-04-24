4b6de5444dba2f50843c6fd35c1dbb02a004e35d
Sacha Dubois <sacha.dubois@icloud.com>
2025-04-24 11:47:31 +0200

    new

--- gen_fabric_project_files.sh
+++ gen_fabric_project_files.sh
@@ -9,23 +9,81 @@
 # CHANGE LOG:
 # 2025-03-15 sdubois Initial version
 #===============================================================================
-# SET STRICT MODE: Makes script safer by handling errors and undefined variables
-set -euo pipefail
-IFS=$'\n\t'
 
 # Resolve the script's directory, handling symlinks if possible
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P 2>/dev/null || pwd -P)
 [ $(basename $SCRIPT_DIR) == "fabric-studio" ] && FABRIC_HOME=$SCRIPT_DIR || FABRIC_HOME=$(dirname $SCRIPT_DIR)
 
+# Local functions
+subhead () { 
+  #printf " ▪ %-60s %s\n" "$1" "$stt" 
+  printf " ▪ %-60s\n" "$1" 
+}
+
 # Define local variables
+
+# Generate devconfig files
+for demo in $(ls -1 $FABRIC_HOME/demos | grep -v zip); do
+[ "fad-ansible-deploy-slb" != "$demo" ] && continue
+  DEMOPATH="${FABRIC_HOME}/demos/${demo}"
+  STOREDIR="${FABRIC_HOME}/files"
+  STAGEDIR=/tmp/fabric_studio_stage
+  [ ! -d $STOREDIR/${demo} ] && mkdir -p $STOREDIR/${demo}
+
+  # Prepare Stage Directory
+  rm -rf $STAGEDIR && mkdir -p $STAGEDIR
+  [ -d $DEMOPATH/fabric ] && cp -r $DEMOPATH/fabric/* $STAGEDIR
+
+  echo ""
+  echo "Building project files for ($demo)"
+
+  # Generate devconfig files
+  if [ -d $DEMOPATH/devconfig ]; then 
+    for device in $(ls -1 $DEMOPATH/devconfig 2>/dev/null); do
+      #BUILDDIR=/tmp/build_${device}_$$
+      BUILDDIR=/tmp/fabric_studio_build_${device}
+      DEVICE_CONFIG=$FABRIC_HOME/devconfig/${device}
+      DEMO_DEVICE_CONFIG=$DEMOPATH/devconfig/${device}
+      DEVICE_CONFIG_FILE=$STOREDIR/${demo}/${device}.tgz
+
+      if [ -f $DEVICE_CONFIG_FILE ]; then
+        stt1=$(find "$DEVICE_CONFIG" -type f -newer "$DEVICE_CONFIG_FILE" | wc -l | awk '{ print $1 }')
+        stt2=$(find "$DEMO_DEVICE_CONFIG" -type f -newer "$DEVICE_CONFIG_FILE" | wc -l | awk '{ print $1 }')
+        if [ $stt1 -eq 0 -a $stt2 -eq 0 ]; then
+          [ -d $STAGEDIR/config/1/DEBIAN ] && cp $DEVICE_CONFIG_FILE $STAGEDIR/config/1/DEBIAN
+          printf " ▪ %-40s %-55s %-15s\n" "Generating Device config for $device" "files/$demo/${device}.tgz"    "no-change"
+          continue
+        fi
+      fi
+
+      rm -rf $BUILDDIR && mkdir -p $BUILDDIR
+      cp -r $DEVICE_CONFIG/* $BUILDDIR
+
+      # Execute fabric demo dedicated configuration script for that device
+      [ -x $DEMO_DEVICE_CONFIG/addons.sh ] && source $DEMO_DEVICE_CONFIG/addons.sh
+
+      OLDDIR=$PWD && cd $BUILDDIR && tar cfz $DEVICE_CONFIG_FILE * && cd $OLDDIR
+      [ -d $STAGEDIR/config/1/DEBIAN ] && cp $DEVICE_CONFIG_FILE $STAGEDIR/config/1/DEBIAN
+
+      printf " ▪ %-40s %-55s %-15s\n" "Generating Device config for $device" "files/$demo/${device}.tgz"    "generated"
+    done
+  fi
+
+  # Pack fabric-studio demo package
+  [ -f $STOREDIR/${demo}.zip ] && rm $STOREDIR/${demo}.zip
+  OLDDIR=$PWD && cd $STAGEDIR && zip -rq $STOREDIR/${demo}.zip * && cd $OLDDIR
+echo "$STOREDIR/${demo}.zip"
+done
+
+exit
 
 echo "Generating fabric studio project files"
 
-# Generate device-config files
-for device in $(ls -1 $FABRIC_HOME/device-config); do
+# Generate devconfig files
+for device in $(ls -1 $FABRIC_HOME/devconfig); do
   BUILDDIR=/tmp/build_${device}_$$
-  DEVICE_CONFIG=$FABRIC_HOME/device-config/${device}
-  DEVICE_CONFIG_FILE=$FABRIC_HOME/files/device-config-${device}.tgz
+  DEVICE_CONFIG=$FABRIC_HOME/devconfig/${device}
+  DEVICE_CONFIG_FILE=$FABRIC_HOME/files/devconfig-${device}.tgz
 
   if [ -f $DEVICE_CONFIG_FILE ]; then 
     stt=$(find "$DEVICE_CONFIG" -type f -newer "$DEVICE_CONFIG_FILE" | wc -l | awk '{ print $1 }')
@@ -36,13 +94,13 @@ for device in $(ls -1 $FABRIC_HOME/device-config); do
   fi
 
   rm -rf $BUILDDIR && mkdir -p $BUILDDIR
-  cp -r $FABRIC_HOME/device-config/${device}/* $BUILDDIR
-  [ -d $BUILDDIR/home/fortinet ] && cp -r $FABRIC_HOME/cert $FABRIC_HOME/corp $BUILDDIR/home/fortinet 
+  cp -r $FABRIC_HOME/devconfig/${device}/* $BUILDDIR
+  [ -d $BUILDDIR/home/fortinet ] && cp -r $FABRIC_HOME/cert $BUILDDIR/home/fortinet 
   [ "$device" == "debian-client" ] && echo "https://raw.githubusercontent.com/pivotal-sadubois/fabric-studio/main/demos/fortinet-sni-based-cert-selection/README.md" > $BUILDDIR/url
-  cd $BUILDDIR && tar cfz $FABRIC_HOME/files/device-config-${device}.tgz * && cd $FABRIC_HOME
+  cd $BUILDDIR && tar cfz $FABRIC_HOME/files/devconfig-${device}.tgz * && cd $FABRIC_HOME
 
-echo "$FABRIC_HOME/files/device-config-${device}.tgz"
-  printf " ▪ %-60s %s\n" "Generating Device config for $device"    "generated"
+echo "$FABRIC_HOME/files/devconfig-${device}.tgz"
+  printf " ▪ %-60s %-30s %s\n" "Generating Device config for $device"    "generated"
 
 done
 
