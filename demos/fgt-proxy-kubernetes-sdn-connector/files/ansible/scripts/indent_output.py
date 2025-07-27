41a27863a2015c42a9bb39587d1dc2a1d0689fdc
Sacha Dubois <sacha.dubois@icloud.com>
2025-07-27 20:56:15 +0200

    deleted old files

--- demos/fgt-proxy-kubernetes-sdn-connector/files/ansible/scripts/indent_output.py
+++
@@ -1,7 +0,0 @@
-import sys
-from rich.console import Console
-
-console = Console()
-
-for line in sys.stdin:
-    console.print("     " + line, end="")
--- devconfig/debadm_MBP14_deCH/home/fortinet/bin/ansible-playbook-setup-employeedb-lb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Generate HTTP Traffice"
-echo ""
-while [ 1 ]; do
-  echo "Press any key to start ..."; read x
-done
